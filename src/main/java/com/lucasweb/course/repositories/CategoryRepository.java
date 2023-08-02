ca702f49f41ea40fb93f156934b6f562799ce066
Lucas <lucas.desenvolvedor@hotmail.com>
2023-08-02 15:37:21 -0300

    Category

---
+++ src/main/java/com/lucasweb/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.lucasweb.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.lucasweb.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+	
+}
